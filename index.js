ac912392208756fbcf569413fe04d3272bf6857d
vuducchinh29 <vuducchinh29@gmail.com>
2022-03-18 11:23:23 +0700

    move init web3 into index

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const Web3 = require("web3")
 const PancakePairABI = require('./abi/pancake_pair_abi.json')
-const {web3_bsc_rpc} = require('./web3')
+const BSC_RPC = 'https://bsc-dataseed.binance.org/'
 const PAIR_ADDRESS = '0x6d7c7beb2111d3332faEbE815C54E005557F3D61' //HFI-BUSD
-const BUSD_ADDRESS = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'
+
+const web3 = new Web3( new Web3.providers.HttpProvider(BSC_RPC))
 
 async function get_price(PAIR_ADDRESS) {
-    const pair_contract = new web3_bsc_rpc.eth.Contract(PancakePairABI, PAIR_ADDRESS)
+    const pair_contract = new web3.eth.Contract(PancakePairABI, PAIR_ADDRESS)
     const reserve = await pair_contract.methods.getReserves().call()
     const raw_amount_0 = reserve._reserve0
     const raw_amount_1 = reserve._reserve1
